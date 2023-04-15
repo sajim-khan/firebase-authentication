9a1c0ba9256a0373f1053c7c5413450912b86aff
sajim-khan <shossain1427@gmail.com>
2023-04-16 04:41:50 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
