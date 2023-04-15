b2141be024c5bee3dae9bc964dbcb81f41fee58f
sajim-khan <shossain1427@gmail.com>
2023-04-16 04:45:37 +0600

    added paragraph

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home page</h2>
-            <h2>This is home page</h2>
-
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is home page</h2>
+      <h2>This is home page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        inventore architecto nihil fugiat sequi alias consectetur voluptas nisi
+        expedita nostrum.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
