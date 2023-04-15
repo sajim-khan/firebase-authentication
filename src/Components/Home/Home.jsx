d0ebd2ad4fba10ff4233ee6a885ca31178aa329f
sajim-khan <shossain1427@gmail.com>
2023-04-16 04:46:19 +0600

    added paragraph

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,6 +10,7 @@ const Home = () => {
         inventore architecto nihil fugiat sequi alias consectetur voluptas nisi
         expedita nostrum.
       </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione soluta dolor perspiciatis commodi, est sint nam alias voluptate deleniti cumque aperiam voluptatem nihil dolorem, architecto odio eligendi impedit aliquid amet.</p>
     </div>
   );
 };
